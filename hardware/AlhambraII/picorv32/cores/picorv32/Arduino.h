4efe72dc89ba917c731d5e6c75efc6bf3f374a1a
ursin <ursin@ThinkPad-T440-ursin>
2019-04-28 07:45:14 +0100

    improvement; * add pin assignment definition              * re-organize folder structure

--- hardware/AlhambraII/picorv32/cores/picorv32/Arduino.h
+++ hardware/AlhambraII/picorv32/cores/picorv32/Arduino.h
@@ -24,7 +24,7 @@ void loop(void);
 #define OUTPUT 0x1
 #define INPUT_PULLUP 0x2
 
-#define LED_BUILTIN 0x0
-
 void pinMode(uint8_t, uint8_t);
 void digitalWrite(uint8_t, uint8_t);
+
+#include "pins_arduino.h"
