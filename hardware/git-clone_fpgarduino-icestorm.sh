efa2a3469c360fc1e5430e97459617477abdf4f7
ursin <ursin@ThinkPad-T440-ursin>
2019-06-06 12:20:16 +0100

    improvement; * added hints on how to use the script also with git together              * ignoring AlhambraII/picorv32/tools/riscv32i/ now

--- hardware/git-clone_fpgarduino-icestorm.sh
+++ hardware/git-clone_fpgarduino-icestorm.sh
@@ -2,6 +2,18 @@
 
 # https://stackoverflow.com/questions/2411031/how-do-i-clone-into-a-non-empty-directory
 
+echo switching this directory to repo https://github.com/drtrigon/fpgarduino-icestorm.git
+echo
+echo in order to switch back use from withing this directory: $ rm -rf .git/
+echo
+echo 'use "git reset HEAD <file>..." to unstage INSTEAD OF'
+echo 'using "git checkout -- <file>..." (to discard changes in working directory)'
+echo
+echo 'use "git diff HEAD <file>..." to diff files'
+echo
+read -n 1 -p "press <(any key)> to continue or <Ctrl+C> to stop..."
+echo
+
 # Clone just the repository's .git folder (excluding files as they are already in
 # `existing-dir`) into an empty temporary directory
 #git clone --no-checkout repo-to-clone existing-dir/existing-dir.tmp # might want --no-hardlinks for cloning local repo
@@ -20,4 +32,9 @@ rmdir fpgarduino-icestorm
 # WARNING: any local changes to the files will be lost.
 #git reset --hard HEAD
 git add AlhambraII
+git add README
+git reset HEAD AlhambraII/picorv32/tools/riscv32i/
+git reset HEAD AlhambraII/picorv32/picorv32-master.zip
+
+git status
 git diff HEAD
